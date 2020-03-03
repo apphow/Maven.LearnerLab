303e5efe60e56f7d4d8a182e4eebfa5f39b80fa1
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-03-02 21:07:38 -0500

    Test Person

--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -1,5 +1,27 @@
 package io.zipcoder.interfaces;
 
 public class Person {
+    final Long id;
+    private String name;
 
+    public Person(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Person(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
